9d29eb5ec9020478b5812dd9695c1111d5ec84b4
a3020 <a3020@fastmail.com>
2021-02-19 18:23:15 +0100

    Version 1.0.4

--- controller.php
+++ controller.php
@@ -11,7 +11,7 @@ final class Controller extends Package
 {
     protected $pkgHandle = 'cache_buster';
     protected $appVersionRequired = '8.3.1';
-    protected $pkgVersion = '1.0.3';
+    protected $pkgVersion = '1.0.4';
     protected $pkgAutoloaderRegistries = [
         'src/CacheBuster' => '\A3020\CacheBuster',
     ];
